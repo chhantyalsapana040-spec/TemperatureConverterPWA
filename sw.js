51965a7a17321cfc82639025a9c27723c5e63f3c
Mukesh Bajgain <bajgainmukesh73@gmail.com>
2025-10-22 12:22:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,32 @@
-const CACHE_NAME = 'temperature-converter-v1';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './converter.js',
-  './converter.css',
-  './manifest.json',
-  './icon192.png',
-  './icon512.png'
-];
-
+const CACHE_NAME = `temperature-converter-v1`;
+// Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      await cache.addAll(FILES_TO_CACHE);
-    })()
-  );
-  self.skipWaiting();
+event.waitUntil((async () => {
+const cache = await caches.open(CACHE_NAME);
+cache.addAll([
+'./',
+'./converter.js',
+'./converter.css'
+]);
+})());
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    (async () => {
-      const cacheNames = await caches.keys();
-      await Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-      await self.clients.claim();
-    })()
-  );
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) return cachedResponse;
-      try {
-        const fetchResponse = await fetch(event.request);
-        cache.put(event.request, fetchResponse.clone());
-        return fetchResponse;
-      } catch (e) {
-        return new Response('You are offline.', {
-          status: 503,
-          statusText: 'Offline'
-        });
-      }
-    })()
-  );
+event.respondWith((async () => {
+const cache = await caches.open(CACHE_NAME);
+// Get the resource from the cache.
+const cachedResponse = await cache.match(event.request);
+if (cachedResponse) {
+return cachedResponse;
+} else {
+try {
+// If the resource was not in the cache, try the network.
+const fetchResponse = await fetch(event.request);
+// Save the resource in the cache and return it.
+cache.put(event.request, fetchResponse.clone());
+return fetchResponse;
+} catch (e) {
+// The network failed.
+}
+}
+})());
 });
