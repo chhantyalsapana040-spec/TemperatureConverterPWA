587db317bf504c9067dea206246a7308513378f7
Mukesh Bajgain <bajgainmukesh73@gmail.com>
2025-10-22 12:15:45 +0530

    change in sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,54 @@
-const CACHE_NAME = `temperature-converter-v1`;
-// Use the install event to pre-cache all initial resources.
+const CACHE_NAME = 'temperature-converter-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/converter.js',
+  '/converter.css',
+  '/manifest.json',
+  '/icon192.png',
+  '/icon512.png'
+];
+
 self.addEventListener('install', event => {
-event.waitUntil((async () => {
-const cache = await caches.open(CACHE_NAME);
-cache.addAll([
-'/',
-'/converter.js',
-'/converter.css'
-]);
-})());
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      await cache.addAll(FILES_TO_CACHE);
+    })()
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    (async () => {
+      const cacheNames = await caches.keys();
+      await Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+      await self.clients.claim();
+    })()
+  );
+});
+
 self.addEventListener('fetch', event => {
-event.respondWith((async () => {
-const cache = await caches.open(CACHE_NAME);
-// Get the resource from the cache.
-const cachedResponse = await cache.match(event.request);
-if (cachedResponse) {
-return cachedResponse;
-} else {
-try {
-// If the resource was not in the cache, try the network.
-const fetchResponse = await fetch(event.request);
-// Save the resource in the cache and return it.
-cache.put(event.request, fetchResponse.clone());
-return fetchResponse;
-} catch (e) {
-// The network failed.
-}
-}
-})());
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request);
+      if (cachedResponse) return cachedResponse;
+      try {
+        const fetchResponse = await fetch(event.request);
+        cache.put(event.request, fetchResponse.clone());
+        return fetchResponse;
+      } catch (e) {
+        return new Response('You are offline.', {
+          status: 503,
+          statusText: 'Offline'
+        });
+      }
+    })()
+  );
 });
